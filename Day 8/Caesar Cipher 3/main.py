c2c7e9629f851292db3df2d9e12c8568b76b2dc7
Harris <khuzaifa743@gmail.com>
2026-03-01 22:03:23 +0500

    Completed ceaser cipther text program

--- Day 8/Caesar Cipher 3/main.py
+++ Day 8/Caesar Cipher 3/main.py
@@ -3,39 +3,47 @@ from art import logo
 
 print(logo)
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
 
 # TODO-2: What happens if the user enters a number/symbol/space?
 
-
 def caesar(original_text, shift_amount, encode_or_decode):
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
     output_text = ""
-    unmodified_text = ""
 
     for letter in original_text:
 
-        if encode_or_decode == "decode":
-            shift_amount *= -1
+        if letter in alphabet:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:
+            output_text += letter
 
-        elif letter not in alphabet:
-            unmodified_text += letter
-
-
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
-
-    print(f"Here is the {encode_or_decode}d result: {output_text}{unmodified_text}")
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
 
 
-# TODO-3: Can you figure out a way to restart the cipher program?
+
+should_continue = True
 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    ask = input("Type 'yes' if you want to go again. Otherwise, type 'no': ")
+
+    if ask == "no":
+        print("Goodbye!")
+        should_continue = False
+
 
 
 
